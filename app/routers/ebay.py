461983248649f37f189416fba4e5162d55f6640d
PrevotYann <yann.prevot19@gmail.com>
2024-05-09 19:47:22 +0200

    Fixing ebay returns

--- app/routers/ebay.py
+++ app/routers/ebay.py
@@ -15,14 +15,15 @@ def ebay_search_query_us(query: str):
     api = BrowseAPI(app_id, cert_id, marketplace_id="EBAY_US")
     responses = api.execute("search", [{"q": query, "limit": 200}])
     if responses is not None:
-        return responses[0]["itemSummaries"]
+        return responses[0].itemSummaries
 
 
 @router.get("/search/fr/", tags=["ebay"])
 def ebay_search_query_france(query: str):
     api = BrowseAPI(app_id, cert_id, marketplace_id="EBAY_FR")
     responses = api.execute("search", [{"q": query, "limit": 200}])
-    return responses[0].itemSummaries
+    if responses is not None:
+        return responses[0].itemSummaries
 
 
 @router.get("/search/fr/condition/{condition}", tags=["ebay"])
@@ -30,7 +31,7 @@ def ebay_search_query_france_with_condition(query: str, condition: str):
     api = BrowseAPI(app_id, cert_id, marketplace_id="EBAY_FR")
     responses = api.execute("search", [{"q": query + " " + condition, "limit": 200}])
     if responses is not None:
-        return responses[0]["itemSummaries"]
+        return responses[0].itemSummaries
 
 
 @router.get("/search/us/condition/{condition}", tags=["ebay"])
@@ -38,4 +39,4 @@ def ebay_search_query_USA_with_condition(query: str, condition: str):
     api = BrowseAPI(app_id, cert_id, marketplace_id="EBAY_US")
     responses = api.execute("search", [{"q": query + " " + condition, "limit": 200}])
     if responses is not None:
-        return responses[0]["itemSummaries"]
+        return responses[0].itemSummaries
