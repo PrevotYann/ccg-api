80c14dc9c715b6d53f35ce9d98dd27c374cf5d00
PrevotYann <yann.prevot19@gmail.com>
2024-09-21 12:49:11 +0200

    Editing to new database server

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:%40Danganronpa47@91.199.227.99:10965/ccgapi?charset=utf8"
+DATABASE_URL = "mysql+pymysql://collecthor_clearlybee:36ce5163b5b3b7d82ec578b256477eed7f01289e@wq1.h.filess.io:3305/collecthor_clearlybee?charset=utf8"
+#DATABASE_URL = "mysql+pymysql://root:%40Danganronpa47@91.199.227.99:10965/ccgapi?charset=utf8"
 
 engine = create_engine(
     DATABASE_URL,
